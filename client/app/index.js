cd1bd420ee1c25125a6ae330e3139748fdfe581c
Stuart <stuart@zooniverse.org>
2014-11-28 14:05:57 -0600

    basic get user media

--- client/app/index.js
+++ client/app/index.js
@@ -1,1 +1,17 @@
-var BinaryClient = require('BinaryClient');
+var BinaryClient = require('binaryjs');
+
+
+navigator.webkitGetUserMedia(
+  {
+    video: true,
+    audio: true
+  },
+  function(localMediaStream){
+    var video = document.querySelector('video');
+    video.src = window.URL.createObjectURL(localMediaStream);
+  },
+  function(err){
+    console.log('didnt work :-(');
+    console.log(err);
+  }
+)
