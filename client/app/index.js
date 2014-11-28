5498fa586d776e0908ebe0a62ffda53a3cd94062
Chris Snyder <bumishness@gmail.com>
2014-11-28 14:36:05 -0600

    play video

--- client/app/index.js
+++ client/app/index.js
@@ -1,14 +1,14 @@
-navigator.webkitGetUserMedia(
-  {
+navigator.webkitGetUserMedia({
     video: true,
-    audio: true
+    audio: false
   },
   function(localMediaStream){
     var video = document.querySelector('video');
     video.src = window.URL.createObjectURL(localMediaStream);
+    video.play()
   },
   function(err){
     console.log('didnt work :-(');
     console.log(err);
   }
-)
+);
